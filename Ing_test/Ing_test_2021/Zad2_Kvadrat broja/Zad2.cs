61bc8683e1cddab9d05f9fdb18a0cda81156806e
Lazar Đorđević <djordjevic.lazar@elfak.rs>
2021-04-07 16:49:01 +0200

    Zad2-3

--- Ing_test/Ing_test_2021/Zad2_Kvadrat broja/Zad2.cs
+++ Ing_test/Ing_test_2021/Zad2_Kvadrat broja/Zad2.cs
@@ -11,11 +11,38 @@ namespace Zad2_Kvadrat_broja
         public static void Main()
         {
             var inputString = Console.ReadLine();
-            var number = int.Parse(inputString);
+            var number = int.Parse(inputString); // 1-4 digits
 
-            // your code goes here
+            int currentVal = number, result = number == 0 ? 9 : 0, currentDigit, multiplier = 1, square, iteration = 0;
+            while(currentVal != 0)
+            {
+                currentDigit = currentVal % 10;
+
+                square = currentDigit == 0 ? 9 : (currentDigit * currentDigit);
+                // counting digits for square
+                int newMultiplier = multiplier;
+                {
+                    int pom = square;
+                    while(pom != 0)
+                    {
+                        pom = pom / 10;
+                        newMultiplier = newMultiplier * 10;
+                    }
+                }
+
+                result = result + ((multiplier * square));
+
+                currentVal = currentVal / 10;
+                multiplier = newMultiplier;
+                iteration++;
+            }
+
+            // sign:
+            if (number < 0) { result = -result; }
+
             // print your results to the console using:
-            Console.WriteLine("your result here");
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 
--- Ing_test/Ing_test_2021/Zad3_Anin dnevnik/Zad3.cs
+++ Ing_test/Ing_test_2021/Zad3_Anin dnevnik/Zad3.cs
@@ -15,9 +15,28 @@ namespace Zad3_Anin_dnevnik
             var firstNumber = int.Parse(inputNumbers[0]);
             var secondNumber = int.Parse(inputNumbers[1]);
 
+            // 3 digit numbers
+            int divNum = 1, result = 0, currentFirst, currentSecond, pom;
+            for (int i = 0; i < 3; i++)
+            {
+                currentFirst = (firstNumber / divNum) % 10;
+                currentSecond = (secondNumber / divNum) % 10;
+
+                pom = Math.Abs(currentFirst - currentSecond);
+
+                if (pom > 5)
+                {
+                    pom = 10 - pom;
+                }
+
+                result = result + pom;
+                divNum = divNum * 10;
+            }
+
             // your code goes here
             // print your results to the console using:
-            Console.WriteLine("your result here");
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 
